53cbdaf019cefdf517b353be7601bd22cef06f17
KevinNandi <362kevin@gmail.com>
2023-10-21 17:06:49 +0300

    home page header
    home page header setup

---
+++ components/HeaderLink.js
@@ -0,0 +1,9 @@
+function HeaderLink() {
+  return (
+    <div>
+      HeaderLink
+    </div>
+  )
+}
+
+export default HeaderLink
--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    domains: ["rb.gy"],
+  }
 }
-
-module.exports = nextConfig
